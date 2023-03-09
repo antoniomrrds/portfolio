b1dc64cb5121f2b8d9d0558fde0d399677d2c0cf
antoniomrrds <arqueiroverd@gmail.com>
2023-03-09 01:42:13 -0300

    finished section projects

--- src/components/Skills/index.tsx
+++ src/components/Skills/index.tsx
@@ -1,11 +1,8 @@
-import React from 'react';
 import { motion } from 'framer-motion';
 import Skill from '../skill';
-type Props = {
-  //
-};
 
-const Skills = (props: Props) => {
+
+const Skills = () => {
   return (
     <motion.div className="h-screen flex relative flex-col text-center md:text-left xl:flex-row max-w-[2000px] xl:px-10 min-h-screen justify-center xl:space-y-0 mx-auto items-center ">
       <h3 className="absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl">
